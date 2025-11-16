09bb568c40c9469cc4f2bf20a084cc245b779a30
MarinoEisenegger <marino.eisenegger@gmail.com>
2025-11-16 17:02:38 +0100

    Add Megatron SSS implementation to benchmark

--- benchmarks/sss_sigmoid_benchmark.py
+++ benchmarks/sss_sigmoid_benchmark.py
@@ -2,13 +2,29 @@ import torch
 from cusss.ops.sss_wrappers import SSS
 from generic_benchmark import benchmark_on_cuda
 
+from megatron.core.jit import jit_fuser
+from megatron.core.transformer.module import MegatronModule
+
 """
 Benchmark our SSS implementations against the PyTorch Sigmoid-module (as well as ReLU as a second baseline for performant activation functions)
 """
 if __name__ == "__main__":
 
+    @jit_fuser
+    def sss(x):
+        return 0.5 * (torch.nn.functional.softsign(x) + 1)
+
+
+    class SSSMegatron(MegatronModule):
+        def __init__(self, config=None):
+            super().__init__(config=config)
+
+        def forward(self, x):
+            return sss(x)
+
+    sss_megatron = SSSMegatron()
     sss_cuda = SSS()
     sigmoid = torch.nn.Sigmoid()  
     relu = torch.nn.ReLU()  
 
-    benchmark_on_cuda(modules={"SSS Cuda": sss_cuda, "ReLU torch": relu}, baseline=("Sigmoid torch", sigmoid))
+    benchmark_on_cuda(modules={"SSS Megatron": sss_megatron, "SSS Cuda Naive": sss_cuda, "ReLU torch": relu}, baseline=("Sigmoid torch", sigmoid))
