aa125b5177aaf19e05cee10f424021bfe907d9ce
MarinoEisenegger <marino.eisenegger@gmail.com>
2025-11-16 17:12:19 +0100

    Add SSS float4 implementation

--- cusss/cuda/src/sss_impl.cu
+++ cusss/cuda/src/sss_impl.cu
@@ -32,6 +32,93 @@ __global__ void sss_backward_kernel(const float* x, const float* grad_out, float
     }
 }
 
+__global__ void sss_forward_kernel_f4(const float* x, float* output, int size) {
+    int tid = blockIdx.x * blockDim.x + threadIdx.x;
+
+    // index in units of float4
+    int i4 = tid;
+    int base = i4 * 4;
+
+    if (base + 3 < size) {
+        // vectorized load
+        float4 v = reinterpret_cast<const float4*>(x)[i4];
+
+        // Explicit scalar lanes
+        float e0 = v.x;
+        float e1 = v.y;
+        float e2 = v.z;
+        float e3 = v.w;
+
+        // elementwise SSS computation
+        float o0 = (e0 * __frcp_rn(1.0f + fabsf(e0))) * 0.5f + 0.5f;
+        float o1 = (e1 * __frcp_rn(1.0f + fabsf(e1))) * 0.5f + 0.5f;
+        float o2 = (e2 * __frcp_rn(1.0f + fabsf(e2))) * 0.5f + 0.5f;
+        float o3 = (e3 * __frcp_rn(1.0f + fabsf(e3))) * 0.5f + 0.5f;
+
+        // pack results
+        float4 out;
+        out.x = o0;
+        out.y = o1;
+        out.z = o2;
+        out.w = o3;
+
+        // vectorized store
+        reinterpret_cast<float4*>(output)[i4] = out;
+    } else {
+        for (int i = 0; i < size - base; i++) {
+            float e = x[base+i];
+            float inv = __frcp_rn(1.0f + fabsf(e));
+            output[base+i] = e * inv * 0.5f + 0.5f;
+        }
+    }
+}
+
+__global__ void sss_backward_kernel_f4(const float* x, const float* grad_out, float* grad_x, int size) {
+    int tid = blockIdx.x * blockDim.x + threadIdx.x;
+
+    // index in units of float4
+    int i4 = tid;
+    int base = i4 * 4;
+
+    if (base + 3 < size) {
+        // vectorized load
+        float4 v = reinterpret_cast<const float4*>(x)[i4];
+        float4 g = reinterpret_cast<const float4*>(grad_out)[i4];
+
+        // Explicit scalar lanes
+        float e0 = v.x;
+        float e1 = v.y;
+        float e2 = v.z;
+        float e3 = v.w;
+
+        // elementwise SSS computation
+        float inv0 = __frcp_rn(1.0f + fabsf(e0));
+        float o0 = (inv0 * inv0) * 0.5f;
+        float inv1 = __frcp_rn(1.0f + fabsf(e1));
+        float o1 = (inv1 * inv1) * 0.5f;
+        float inv2 = __frcp_rn(1.0f + fabsf(e2));
+        float o2 = (inv2 * inv2) * 0.5f;
+        float inv3 = __frcp_rn(1.0f + fabsf(e3));
+        float o3 = (inv3 * inv3) * 0.5f;
+
+        // pack results
+        float4 out;
+        out.x = g.x * o0;
+        out.y = g.y * o1;
+        out.z = g.z * o2;
+        out.w = g.w * o3;
+
+        // vectorized store
+        reinterpret_cast<float4*>(grad_x)[i4] = out;
+    } else {
+        for (int i = 0; i < size - base; i++) {
+            float e = x[base+i];
+            float g = grad_out[base+i];
+            float inv = __frcp_rn(1.0f + fabsf(e));
+            grad_x[base+i] = g * (inv * inv) * 0.5f;
+        }
+    }
+}
 
 // ===================================================================
 // FORWARD AND BACKWARD IMPLEMENTATIONS
@@ -80,6 +167,51 @@ std::vector<torch::Tensor> backward_cuda(torch::Tensor &x, torch::Tensor &grad_o
     return {grad_x};
 }
 
+torch::Tensor forward_cuda_f4(torch::Tensor &x) {
+    TORCH_CHECK(x.dtype() == torch::kFloat, "Input tensor must be float!");
+    TORCH_CHECK(x.is_cuda(), "Input tensor must be a CUDA tensor!");
+
+    x = x.contiguous();
+    auto output = torch::empty_like(x).contiguous();
+    int size = x.numel();
+    
+    // @TODO: Better kernel launch configuration
+    int blockSize = 256;
+    int numBlocks = (size/4 + blockSize - 1) / blockSize;
+
+    sss_forward_kernel_f4<<<numBlocks, blockSize>>>(
+        x.data_ptr<float>(), output.data_ptr<float>(), size
+    );
+
+    return output;
+}
+
+std::vector<torch::Tensor> backward_cuda_f4(torch::Tensor &x, torch::Tensor &grad_outputs) {
+    TORCH_CHECK(x.dtype() == torch::kFloat, "Input tensor must be float!");
+    TORCH_CHECK(x.is_cuda(), "Input tensor must be a CUDA tensor!");
+    TORCH_CHECK(grad_outputs.dtype() == torch::kFloat, "Grad tensor must be float!");
+    TORCH_CHECK(grad_outputs.is_cuda(), "Grad tensor must be a CUDA tensor!");
+    TORCH_CHECK(x.numel() == grad_outputs.numel(), "Grad tensor must be a CUDA tensor!");
+
+    grad_outputs = grad_outputs.contiguous(); // Apparently since the loss output might be non-contiguous
+    x = x.contiguous();
+    auto grad_x = torch::empty_like(x).contiguous();
+    int size = x.numel();
+
+    // @TODO: Better kernel launch configuration
+    int blockSize = 256;
+    int numBlocks = (size/4 + blockSize - 1) / blockSize;
+
+    sss_backward_kernel_f4<<<numBlocks, blockSize>>>(
+        x.data_ptr<float>(),
+        grad_outputs.data_ptr<float>(),
+        grad_x.data_ptr<float>(),
+        size
+    );
+
+    return {grad_x};
+}
+
 
 // ===================================================================
 // AUTOGRAD CLASS DEFINITIONS
@@ -90,6 +222,17 @@ torch::Tensor SSSAutograd::forward(AutogradContext *ctx, Tensor x) {
 }
 
 variable_list SSSAutograd::backward(AutogradContext *ctx, variable_list grad_outputs) {
+    auto saved = ctx->get_saved_variables();
+    auto x = saved[0];
+    return backward_cuda(x, grad_outputs[0]);
+}
+
+torch::Tensor SSSAutograd_f4::forward(AutogradContext *ctx, Tensor x) {
+    ctx->save_for_backward({x});
+    return forward_cuda_f4(x);
+}
+
+variable_list SSSAutograd_f4::backward(AutogradContext *ctx, variable_list grad_outputs) {
     auto saved = ctx->get_saved_variables();
     auto x = saved[0];
     return backward_cuda(x, grad_outputs[0]);
--- tests/test_sss.py
+++ tests/test_sss.py
@@ -1,19 +1,21 @@
 import pytest
 import torch
 
-from cusss.ops.sss_wrappers import SSS
+from cusss.ops.sss_wrappers import SSS, SSS_f4
 
 @pytest.fixture
 def sss_setup():
     """Fixture to set up SSS instances and test input"""
     device = torch.device("cuda")
     
-    x = torch.randn(10, 10, device=device, requires_grad=True)
+    x = torch.randn(64, 512, device=device, requires_grad=True)
     sss = SSS().to(device)
+    sss_f4 = SSS_f4().to(device)
 
     return {
         "x": x,
         "sss": sss,
+        "sss_float4": sss_f4
     }
 
 def test_forward(sss_setup):
@@ -22,6 +24,11 @@ def test_forward(sss_setup):
     model = sss_setup["sss"]
     output = model(input)
     torch.testing.assert_close(output, 0.5 * (input / (1.0 + input.abs()) + 1.0))
+    
+    # Also test cuda float4 kernel
+    model_f4 = sss_setup["sss_float4"]
+    output_f4 = model_f4(input)
+    torch.testing.assert_close(output_f4, 0.5 * (input / (1.0 + input.abs()) + 1.0))
 
 def test_backward(sss_setup):
     """Compare CUDA backward output to PyTorch implementation"""
@@ -38,3 +45,17 @@ def test_backward(sss_setup):
     grad_ref = 0.5 / (1.0 + input.detach().abs()).pow(2)
 
     torch.testing.assert_close(grad_cuda, grad_ref)
+
+    # Also test cuda float4 kernel
+    input.grad = None
+    model_f4 = sss_setup["sss_float4"]
+
+    output_f4 = model_f4(input)
+
+    loss_f4 = output_f4.sum()
+    loss_f4.backward()
+
+    grad_cuda_f4 = input.grad.clone()
+    grad_ref = 0.5 / (1.0 + input.detach().abs()).pow(2)
+
+    torch.testing.assert_close(grad_cuda_f4, grad_ref)
