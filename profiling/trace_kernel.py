0b4a963aaed6360a3a47a61b77930c838f0324b6
ucinereo <nicola.studer2000@gmail.com>
2025-11-22 20:48:02 +0100

    [SSS, WIP] Add profiling scripts

---
+++ profiling/trace_kernel.py
@@ -0,0 +1,37 @@
+import torch
+import torch.cuda.nvtx as nvtx
+
+from cusss.ops.sss_wrappers import SSS
+
+# 1. SETUP
+device = torch.device("cuda")
+x = torch.randn(64, 1_000_000, device=device)
+sss = SSS().to(device)
+sigmoid = torch.nn.Sigmoid().to(device)
+
+print("Warming up...")
+for _ in range(20):
+    _ = sss(x)
+    _ = sigmoid(x)
+torch.cuda.synchronize()
+
+print("Starting trace...")
+torch.cuda.profiler.start()
+
+# --- REGION A: PyTorch Native ---
+nvtx.range_push("A: PyTorch Native")
+output_native = sigmoid(x)
+torch.cuda.synchronize() # Wait for it to finish before popping the marker
+del output_native
+nvtx.range_pop()
+
+# --- REGION B: Custom Kernel ---
+nvtx.range_push("B: Custom Kernel")
+# output_custom = my_custom_kernel(x, y)
+output_custom = sss(x)
+torch.cuda.synchronize()
+nvtx.range_pop()
+
+# 4. STOP TRACE
+torch.cuda.profiler.stop()
+print("Done.")
