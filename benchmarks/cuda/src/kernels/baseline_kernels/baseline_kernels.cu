e13e3331cc1b803076a0e18f168329cdc5a6f821
ucinereo <nicola.studer2000@gmail.com>
2025-11-22 14:40:33 +0100

    [Benchmark] Add instruction source

--- benchmarks/cuda/src/kernels/baseline_kernels/baseline_kernels.cu
+++ benchmarks/cuda/src/kernels/baseline_kernels/baseline_kernels.cu
@@ -1,3 +1,4 @@
+// Instruction set copied from: https://github.com/pytorch/pytorch/blob/a9cb5bc90b59a23f38b1f9943af1616b9bb80ce4/aten/src/ATen/native/cuda/UnarySpecialOpsKernel.cu#L123
 __global__ void sigmoid_forward_kernel(const float* x, float* output, int size) {
   int idx = blockIdx.x * blockDim.x + threadIdx.x;
   if (idx < size) {
